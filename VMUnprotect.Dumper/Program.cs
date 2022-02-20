5f1ad9a84eff89d798411332c80ed11b92389d05
void <47751114+void-stack@users.noreply.github.com>
2022-02-20 17:16:47 +0100

    Files push.

---
+++ VMUnprotect.Dumper/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsmResolver.IO;
+using AsmResolver.PE.File;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+const string asciiArt = @"
+_________                __                 
+\_   ___ \  ____________/  |_  ____ ___  ___
+/    \  \/ /  _ \_  __ \   __\/ __ \\  \/  /
+\     \___(  <_> )  | \/|  | \  ___/ >    < 
+ \______  /\____/|__|   |__|  \___  >__/\_ \
+        \/                        \/      \/
+                VMUnprotect.Dumper
+          https://github.com/void-stack
+            Credits: wwh1004, MrToms
+";
+                
+Console.Title = "VMUnprotect.Dumper";
+Console.WriteLine(asciiArt);
+
+if (args.Length > 0 && File.Exists(args[0])) {
+    var target = args[0];
+    var output = $"{Path.GetFileNameWithoutExtension(target)}-decrypted.exe";
+    var assembly = Assembly.LoadFile(target);
+    var moduleHandle = assembly.ManifestModule.ModuleHandle;
+
+    Console.WriteLine("[+] Decrypting methods");
+    RuntimeHelpers.RunModuleConstructor(moduleHandle);
+    var hInstanceFixed = Marshal.GetHINSTANCE(assembly.ManifestModule);
+
+    Console.WriteLine("[+] Reading decrypted module");
+    var decryptedPeFile = PEFile.FromModuleBaseAddress(hInstanceFixed);
+
+    foreach (var section in decryptedPeFile.Sections)
+        Console.WriteLine("[+] Sections: " + section.Name);
+
+    Console.WriteLine("[+] Writing file");
+    using (var fs = File.Create(output)) {
+        decryptedPeFile.Write(new BinaryStreamWriter(fs));
+    }
+
+    Console.WriteLine("[+] Decrypted all methods!");
+}
+else 
+    Console.WriteLine("File either doesn't exist or you didn't provide it (VMProtect.Dumper File.exe)");
+
+Console.ReadKey();
---
+++ VMUnprotect.Dumper/README.md
@@ -0,0 +1,36 @@
+<p align="center">
+  <img width="256" heigth="256" src="docs\vmup.png">
+<h1 align="center">VMUnprotect.Dumper</h1>
+<p align="center">
+  <strong>VMUnprotect.Dumper</strong> is a project engaged in hunting tampered <a href="https://vmpsoft.com">VMProtect</a> assemblies. It makes use of <a href="https://github.com/pardeike/Harmony">AsmResolver</a> to dynamically untamper <strong>VMP</strong> protected assembly. Works on <a href="https://vmpsoft.com/20210919/vmprotect-3-5-1/">VMProtect 3.5.1</a> (Latest) and few versions back.
+</p>
+</p>
+<p align="center">
+  <img src="https://forthebadge.com/images/badges/built-with-love.svg" alt="appveyor-ci" />
+  <img src="https://forthebadge.com/images/badges/made-with-c-sharp.svg" alt="appveyor-ci" />
+</p>
+</p>
+
+</br></br>
+# Before and after usage of VMUnprotect.Dumper
+<img src="docs/protected.png" width="49%" height="390px" style="display:inline;">
+<div style="display:inline;width:5px;"></div>
+<img src="docs/decrypted.png" width="49%" height="390px" style="display:inline;">
+
+
+# Usage
+```sh
+VMUnprotect.Dumper.exe example.vmp.exe
+```
+
+
+
+# Credits
+* [wwh1004](https://github.com/wwh1004) - Idea.
+* [(Discord) MrToms#1244]() - Resources
+
+This tool uses the following (open source) software:
+* [AsmResolver](https://github.com/Washi1337/AsmResolver) by [Washi](https://github.com/Washi1337), licensed under the MIT license, for reading/writing assemblies.
+
+## Want to support this project?
+BTC: bc1q048wrqztka5x2syt9mtj68uuf73vqry60s38vf
